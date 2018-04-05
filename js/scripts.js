4e269ff4fd0af47fb86e4b435ecea6d11531563c
Dawn Mott and Matt Dowdney <dawn@>
2018-04-05 14:09:49 -0700

    add funtion to push squares into arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,7 @@
 // Business Logic
+var arrayX = [];
+var arrayO = [];
+
 function Player (name, markType){
   this.name = name;
   this.marktype = markType;
@@ -22,12 +25,18 @@ Game.prototype.playerSwitch = function() {
   }
 }
 
+ function arrayXPush(number) {
+   if (Space.mark === "X")
+   arrayX.push(number);
+ }
+
+
 //UI Logic
 $(document).ready(function(){
   var playerOne = new Player ("", "X");
   var playerTwo = new Player ("", "O");
   var newGame = new Game ("X")
-  var boardArray =[];
+  // var boardArray =[];
 //name entry behavior
   $("#player-1").submit(function(event){
     event.preventDefault();
@@ -47,8 +56,13 @@ $(document).ready(function(){
     $("#img0").attr("src","img/donut.jpeg");
     }
     var a = new Space(0, newGame.turn)
-    boardArray.push(a);
+    if (a.mark === "X"){
+      arrayX.push(a.coordinate);
+    } else {
+      arrayO.push(a.coordinate);
+    }
     newGame.playerSwitch();
+    // console.log(boardArray);
   });
 
   $(".b").one("click", function(){
@@ -56,6 +70,12 @@ $(document).ready(function(){
       $("#img1").attr("src","img/x1.png");
     } else {
     $("#img1").attr("src","img/donut.jpeg");
+    }
+    var b = new Space(1, newGame.turn)
+    if (b.mark === "X"){
+      arrayX.push(b.coordinate);
+    } else {
+      arrayO.push(b.coordinate);
     }
     newGame.playerSwitch();
   });
@@ -65,14 +85,27 @@ $(document).ready(function(){
       $("#img2").attr("src","img/x1.png");
     } else {
     $("#img2").attr("src","img/donut.jpeg");
+  }
+    var c = new Space(2, newGame.turn)
+    if (c.mark === "X"){
+      arrayX.push(c.coordinate);
+    } else {
+      arrayO.push(c.coordinate);
     }
     newGame.playerSwitch();
   });
+
   $(".d").one("click", function(){
     if(newGame.turn === "X"){
       $("#img3").attr("src","img/x1.png");
     } else {
     $("#img3").attr("src","img/donut.jpeg");
+    }
+    var d = new Space(3, newGame.turn)
+    if (d.mark === "X"){
+      arrayX.push(d.coordinate);
+    } else {
+      arrayO.push(d.coordinate);
     }
     newGame.playerSwitch();
   });
@@ -82,6 +115,12 @@ $(document).ready(function(){
     } else {
     $("#img4").attr("src","img/donut.jpeg");
     }
+    var e = new Space(4, newGame.turn)
+    if (e.mark === "X"){
+      arrayX.push(e.coordinate);
+    } else {
+      arrayO.push(e.coordinate);
+    }
     newGame.playerSwitch();
   });
   $(".f").one("click", function(){
@@ -89,6 +128,12 @@ $(document).ready(function(){
       $("#img5").attr("src","img/x1.png");
     } else {
     $("#img5").attr("src","img/donut.jpeg");
+    }
+    var f = new Space(5, newGame.turn)
+    if (f.mark === "X"){
+      arrayX.push(f.coordinate);
+    } else {
+      arrayO.push(f.coordinate);
     }
     newGame.playerSwitch();
   });
@@ -98,6 +143,12 @@ $(document).ready(function(){
     } else {
     $("#img6").attr("src","img/donut.jpeg");
     }
+    var g = new Space(6, newGame.turn)
+    if (g.mark === "X"){
+      arrayX.push(g.coordinate);
+    } else {
+      arrayO.push(g.coordinate);
+    }
     newGame.playerSwitch();
   });
   $(".h").one("click", function(){
@@ -105,6 +156,12 @@ $(document).ready(function(){
       $("#img7").attr("src","img/x1.png");
     } else {
     $("#img7").attr("src","img/donut.jpeg");
+    }
+    var h = new Space(7, newGame.turn)
+    if (h.mark === "X"){
+      arrayX.push(h.coordinate);
+    } else {
+      arrayO.push(h.coordinate);
     }
     newGame.playerSwitch();
   });
@@ -114,6 +171,15 @@ $(document).ready(function(){
     } else {
     $("#img8").attr("src","img/donut.jpeg");
     }
+    var i = new Space(8, newGame.turn)
+    if (i.mark === "X"){
+      arrayX.push(i.coordinate);
+    } else {
+      arrayO.push(i.coordinate);
+    }
     newGame.playerSwitch();
   });
-})
+  console.log(arrayX);
+  console.log(arrayO);
+
+});
